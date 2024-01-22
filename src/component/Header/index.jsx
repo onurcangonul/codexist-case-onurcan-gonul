ea30068ee0fbc16d282980c002ab32b2a2f22879
onurcangonul <onurcangonul@gmail.com>
2024-01-23 02:10:47 +0300

    header fixed

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -18,10 +18,10 @@ const Header = () => {
           <Link to="/" className="flex items-center">
             <img src={logo} className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
           </Link>
-          <div className="flex items-center lg:order-2">
+          <div className="hidden md:flex items-center lg:order-2">
             <Link
               to="/about-us"
-              className="text-gray-800 dark:text-white hover:bg-gray-50  font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+              className="text-gray-800 dark:text-white hover:bg-gray-50 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
             >
               About Us
             </Link>
@@ -31,35 +31,37 @@ const Header = () => {
             >
               Cart
             </Link>
-
-            <button
-              onClick={toggleMobileMenu}
-              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="mobile-menu-2"
-              aria-expanded={isMobileMenuOpen}
-            >
-              <span className="sr-only">Open main menu</span>
-              <RxHamburgerMenu color="#07416E" size={24} />
-            </button>
           </div>
+
+          <button
+            onClick={toggleMobileMenu}
+            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="mobile-menu-2"
+            aria-expanded={isMobileMenuOpen}
+          >
+            <span className="sr-only">Open main menu</span>
+            <RxHamburgerMenu color="#07416E" size={24} />
+          </button>
           <div
-            className={`lg:flex lg:items-center w-full lg:w-auto lg:order-1 ${
+            className={`lg:hidden w-full ${
               isMobileMenuOpen ? "block" : "hidden"
             }`}
             id="mobile-menu-2"
           >
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <ul className="flex flex-col mt-4 font-medium">
               <li>
                 <Link
                   to="/about-us"
-                  className="block py-2 pr-4 pl-3 bg-[#07416E]  text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                  className="block py-2 pr-4 pl-3 bg-[#07416E] text-white rounded bg-primary-700 dark:text-white"
                   aria-current="page"
                 >
                   About Us
                 </Link>
+              </li>
+              <li>
                 <Link
                   to="/cart"
-                  className="block py-2 pr-4 pl-3 mt-2 bg-[#07416E]  text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+                  className="block py-2 pr-4 pl-3 mt-2 bg-[#07416E] text-white rounded bg-primary-700 dark:text-white"
                   aria-current="page"
                 >
                   Cart
